5fb71398b33976c9d2d90db64e1bf429cba78389
riesha <seleneia@outlook.jp>
2023-05-28 01:43:16 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>goth.zip</title>
+</svelte:head>
+<main>
+  <section>
+    <h1>˗ˏˋ𓆩†𓆪ˊˎ˗</h1>
+  </section>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+
+  h1 {
+    font-size: 8rem;
+    color: rgb(12, 12, 12);
+    padding: 0;
+    text-align: center;
+  }
+  p {
+    color: aliceblue;
+    opacity: 40%;
+  }
+</style>
